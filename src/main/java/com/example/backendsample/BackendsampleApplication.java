ea67c264aef9eb07c85ae74c4c155c3a46458b4e
HyeonJun Jeon <teddy3582@gmail.com>
2022-08-10 04:20:20 +0900

    Init

---
+++ src/main/java/com/example/backendsample/BackendsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendsampleApplication.class, args);
+	}
+
+}
